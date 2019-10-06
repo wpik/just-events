f1f5bef2b5fcb5b342baa8ca269bcd11db2ae6a8
wpik <wpik256@gmail.com>
2019-10-06 21:13:18 +0200

    Fresh spring project

---
+++ src/main/java/com/github/wpik/justevents/JustEventsApplication.java
@@ -0,0 +1,13 @@
+package com.github.wpik.justevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustEventsApplication.class, args);
+	}
+
+}
