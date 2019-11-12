75c33280b4f06b5bd3d752e4d7aec2a28d66654d
wpik <wpik256@gmail.com>
2019-11-12 22:00:26 +0100

    Tests rewritten with junit5

--- src/test/java/com/github/wpik/justevents/JustEventsTest.java
+++ src/test/java/com/github/wpik/justevents/JustEventsTest.java
@@ -5,84 +5,73 @@ import com.github.wpik.justevents.events.NestedPayload;
 import com.github.wpik.justevents.events.SomePayload;
 import com.github.wpik.justevents.events.ValidEvents;
 import com.github.wpik.justevents.exception.JustEventDeserializationException;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import javax.validation.ConstraintViolationException;
-import java.util.Arrays;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class JustEventsTest {
+class JustEventsTest {
     private JustEvents justEvents;
 
-    @Before
-    public void setup() {
+    @BeforeEach
+    void setup() {
         justEvents = new JustEvents();
         justEvents.registerEvent(SomePayload.NAME, SomePayload.class);
         justEvents.registerEvent(NestedPayload.NAME, NestedPayload.class);
     }
 
-    @Test
-    public void validEventSerializationTest() {
-        Arrays.stream(ValidEvents.values()).forEach(e -> {
-            String json = justEvents.serialize(e.getEvent());
-            assertEquals(e.getJson(), json);
-        });
+    @ParameterizedTest
+    @EnumSource(ValidEvents.class)
+    void validEventSerializationTest(ValidEvents validEvent) {
+        String json = justEvents.serialize(validEvent.getEvent());
+        assertEquals(validEvent.getJson(), json);
+    }
+
+    @ParameterizedTest
+    @EnumSource(ValidEvents.class)
+    void validEventDeserializationTest(ValidEvents validEvent) {
+        Event<?> event = justEvents.deserialize(validEvent.getJson());
+        assertEquals(validEvent.getEvent(), event);
+    }
+
+    @ParameterizedTest
+    @EnumSource(ValidEvents.class)
+    void validEventNameDeserializationTest(ValidEvents validEvent) {
+        String name = justEvents.deserializeEventName(validEvent.getJson());
+        assertEquals(validEvent.getEvent().getName(), name);
     }
 
     @Test
-    public void validEventDeserializationTest() {
-        Arrays.stream(ValidEvents.values()).forEach(e -> {
-            Event<?> event = justEvents.deserialize(e.getJson());
-            assertEquals(e.getEvent(), event);
+    void deserializationWithoutRegisteredEventFailsTest() {
+        assertThrows(JustEventDeserializationException.class, () -> {
+            justEvents = new JustEvents();
+            justEvents.deserialize(ValidEvents.SOME_PAYLOAD.getJson());
         });
     }
 
-    @Test
-    public void validEventNameDeserializationTest() {
-        Arrays.stream(ValidEvents.values()).forEach(e -> {
-            String name = justEvents.deserializeEventName(e.getJson());
-            assertEquals(e.getEvent().getName(), name);
-        });
+    @ParameterizedTest
+    @EnumSource(InvalidEvents.class)
+    void invalidEventNameDeserializationTest(InvalidEvents invalidEvent) {
+        String name = justEvents.deserializeEventName(invalidEvent.getJson());
+        assertEquals(invalidEvent.getEvent().getName(), name);
     }
 
-    @Test(expected = JustEventDeserializationException.class)
-    public void deserializationWithoutRegisteredEventFailsTest() {
-        justEvents = new JustEvents();
-        justEvents.deserialize(ValidEvents.SOME_PAYLOAD.getJson());
+    @ParameterizedTest
+    @EnumSource(InvalidEvents.class)
+    void invalidEventSerializationTest(InvalidEvents invalidEvent) {
+        assertThrows(ConstraintViolationException.class, () -> justEvents.serialize(invalidEvent.getEvent()));
     }
 
-    @Test
-    public void invalidEventNameDeserializationTest() {
-        Arrays.stream(InvalidEvents.values()).forEach(e -> {
-            String name = justEvents.deserializeEventName(e.getJson());
-            assertEquals(e.getEvent().getName(), name);
-        });
-    }
+    @ParameterizedTest
+    @EnumSource(InvalidEvents.class)
+    void invalidEventDeserializationTest(InvalidEvents invalidEvent) {
+        Throwable throwable = assertThrows(RuntimeException.class, () -> justEvents.deserialize(invalidEvent.getJson()));
 
-    @Test
-    public void invalidEventSerializationTest() {
-        Arrays.stream(InvalidEvents.values()).forEach(e -> {
-            try {
-                justEvents.serialize(e.getEvent());
-                fail("serialization should fail for " + InvalidEvents.class.getName() + "." + e.name());
-            } catch (ConstraintViolationException ex) {
-                assertTrue(true);
-            }
-        });
-    }
-
-    @Test
-    public void invalidEventDeserializationTest() {
-        Arrays.stream(InvalidEvents.values()).forEach(e -> {
-            try {
-                justEvents.deserialize(e.getJson());
-                fail("deserialization should fail for " + InvalidEvents.class.getName() + "." + e.name());
-            } catch (Exception ex) {
-                assertEquals("expected exception should be caught for " + InvalidEvents.class.getName() + "." + e.name(),
-                        e.getDeserializationExpectedException(), ex.getClass());
-            }
-        });
+        assertEquals(invalidEvent.getDeserializationExpectedException(), throwable.getClass());
     }
 }
